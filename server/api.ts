b5b73b122c1d11ba9c81b6a2617a37509ed36557
Ismailov-M11 <121032887+Ismailov-M11@users.noreply.github.com>
2025-08-21 15:25:04 +0500

    Delete server/api.ts

--- server/api.ts
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import { createServer } from "../server";  // путь к твоему createServer()
-
-// Создаём express-приложение
-const app = createServer();
-
-// Экспортируем handler для Vercel
-export default serverless(app);
